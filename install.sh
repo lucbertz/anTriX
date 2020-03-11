741a6eed895efc26a4ebba30dd93894e2cb028d1
lucbertz <61161236+lucbertz@users.noreply.github.com>
2020-03-11 17:34:06 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade --yes
+
+wget http://mirror.ppa.trinitydesktop.org/trinity/deb/trinity-keyring.deb
+sudo dpkg -i trinity-keyring.deb
+apt install -f --yes
+
+echo deb http://mirror.ppa.trinitydesktop.org/trinity/deb/trinity-v3.5.13 buster main > /etc/apt/sources.list
+echo deb http://mirror.ppa.trinitydesktop.org/trinity/deb/trinity-builddeps-v3.5.13 buster main >> /etc/apt/sources.list
+
+apt update
+
+apt install --yes --no-install-recommends firmware-linux-free firmware-linux-nonfree \
+   firmware-atheros firmware-iwlwifi firmware-zd1211 firmware-realtek firmware-bnx2 firmware-brcm80211 firmware-b43-* b43-fwcutter\
+   firmware-cavium firmware-ipw2x00 firmware-libertas firmware-ti-connectivity \
+   xserver-xorg xserver-xorg-video-* xserver-xorg-input-all xserver-xorg-legacy \
+   wmctrl xdotool libdrm-intel1 libgl1-mesa-dri libglu1-mesa \
+   xinit x11-utils x11-xserver-utils libxcursor1 \
+   breeze-cursor-theme
+
+rm -Rf /usr/share/icons/breeze_cursors
+mv /usr/share/icons/Breeze_Snow /usr/share/icons/breeze_cursors
+
+apt install tdebase-trinity
